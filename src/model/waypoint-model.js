e92856cff84abe23477125bfc13f9db5b2816d1e
lisakolts <crokodile.liza@gmail.com>
2024-04-30 18:37:34 +0300

    2.2 Создание и подключение модели

--- src/model/waypoint-model.js
+++ src/model/waypoint-model.js
@@ -2,7 +2,7 @@ import { getRandomWaypoint } from '../mock/waypoint-mock.js';
 
 const WAYPOINT_COUNT = 5;
 
-export default class WaypointModel {
+export default class WaypoinstModel {
   waypoints = Array.from({length: WAYPOINT_COUNT}, getRandomWaypoint);
 
   getWaypoint() {
--- src/presenter/list-presenter.js
+++ src/presenter/list-presenter.js
@@ -10,17 +10,24 @@ export default class ListPresenter {
   listComponent = new ListView();
   listElementComponent = new ListElementView();
 
-  constructor({listContainer}) {
+  constructor({listContainer, waypointsModel}) {
     this.listContainer = listContainer;
+    this.waypointsModel = waypointsModel;
   }
 
   init() {
+    this.listWaypoints = [...this.waypointsModel.getWaypoint()];
+
     render(this.listComponent, this.listContainer);
     render(new AddFormView(), this.listContainer, 'afterbegin');
     render(this.listElementComponent, this.listComponent.getElement());
     render(new EditFormView(), this.listElementComponent.getElement());
 
     //render(new ListElementView(), this.listElementComponent.getElement());
+
+    for (let i = 0; i < this.listWaypoints.length; i++) {
+      render(new ListView({task: this.listWaypoints[i]}), this.listElementComponent.getElement());
+    }
 
     // for (let i = 0; i < 3; i++) {
     //   render(new ListElementView(), this.listElementComponent.getElement());
