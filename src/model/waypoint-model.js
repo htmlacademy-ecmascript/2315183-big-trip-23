e79b7fb1f4814fd9d2484a3c31fca63b21be1f00
lisakolts <crokodile.liza@gmail.com>
2024-05-07 16:17:49 +0300

    4.1.3 Добавление полям приватности и добавление геттеров

--- src/model/waypoint-model.js
+++ src/model/waypoint-model.js
@@ -3,9 +3,9 @@ import { getRandomWaypoint } from '../mock/waypoint-mock.js';
 const WAYPOINT_COUNT = 5;
 
 export default class WaypoinstModel {
-  waypoints = Array.from({length: WAYPOINT_COUNT}, getRandomWaypoint);
+  #waypoints = Array.from({length: WAYPOINT_COUNT}, getRandomWaypoint);
 
-  getWaypoint() {
-    return this.waypoints;
+  get waypoint() {
+    return this.#waypoints;
   }
 }
--- src/presenter/list-presenter.js
+++ src/presenter/list-presenter.js
@@ -1,42 +1,41 @@
 import ListView from '../view/list-view.js';
 import ListElementView from '../view/list-element-view.js';
 import AddFormView from '../view/add-form-view.js';
-import ContainerListView from '../view/container-list-view.js';
 import ListOfferElementView from '../view/list-offer-element-view.js';
 import { render } from '../framework/render.js';
 
 export default class ListPresenter {
-  container = new ContainerListView;
-  listComponent = new ListView();
+  #listContainer = null;
+  #waypointsModel = null;
 
+  #listComponent = new ListView();
+
+  #listWaypoints = [];
 
   constructor({listContainer, waypointsModel}) {
-    this.listContainer = listContainer;
-    this.waypointsModel = waypointsModel;
+    this.#listContainer = listContainer;
+    this.#waypointsModel = waypointsModel;
   }
 
   init() {
-    this.listWaypoints = [...this.waypointsModel.getWaypoint()];
+    this.#listWaypoints = [...this.#waypointsModel.waypoint];
 
-    render(this.listComponent, this.listContainer);
+    render(this.#listComponent, this.#listContainer);
 
-    // Мне это нужно для примера
-    //render(new AddFormView(), this.listComponent.getElement(), 'afterbegin');
-    // render(this.listComponent, this.listComponent.getElement());
-    // render(new EditFormView(), this.listComponent.getElement());
-    //render(new ListElementView(), this.listElementComponent.getElement());
+    for (let i = 0; i < this.#listWaypoints.length; i++) {
+      const listElementComponent = new ListElementView({listElement: this.#listWaypoints[i]});
 
-    for (let i = 0; i < this.listWaypoints.length; i++) {
-      const listElementComponent = new ListElementView({listElement: this.listWaypoints[i]});
+      render(listElementComponent, this.#listComponent.element);
 
-      render(listElementComponent, this.listComponent.element);
-
-      for (let j = 0; j < this.listWaypoints[i].offers.length; j++) {
-        render(new ListOfferElementView({offerElement: this.listWaypoints[i].offers[j]}), listElementComponent.element.querySelector('.event__selected-offers'));
+      if(i === 0) {
+        render(new AddFormView({addFormElement: this.#listWaypoints[0]}), this.#listComponent.element);
       }
 
-      render(new AddFormView({addFormElement: this.listWaypoints[i]}), this.listComponent.element);
-
+      // Может ещё понадобится
+      for (let j = 0; j < this.#listWaypoints[i].offers.length; j++) {
+        render(new ListOfferElementView({offerElement: this.#listWaypoints[i].offers[j]}), listElementComponent.element.querySelector('.event__selected-offers'));
+      }
     }
+
   }
 }
